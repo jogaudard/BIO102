fe328b017c3e4716c553e1f6cfc610ef66a7e780
Malene Dekke Eik <malene.eik@student.uib.no>
2020-10-05 09:31:26 +0200

    changed some of the code and the look of the tables,

--- flux_bio102.r
+++ flux_bio102.r
@@ -1,6 +1,12 @@
 #Installing and importing tidyverse 
 #install.packages("tidyverse")
 library(tidyverse)
+library(dbplyr)
+
+#Installing and getting devtools and datadownloader
+#install.packages("devtools") #Install this if you don't have it on your computer already
+library(devtools)
+devtools::install_github("Between-the-Fjords/dataDownloader")
 library("dataDownloader") #check https://github.com/Between-the-Fjords/dataDownloader it is a bit tricky to install
 # datadownloader allows you to download data from OSF. It will check if the datas you have locally are older than the one on OSF. If needed, the new data will be downloaded.
 
@@ -8,10 +14,11 @@ get_file(node = "3qhdj",
          file = "fluxes_bio102.csv", #this is the file on OSF
          remote_path = "")
 
+
 #opening data files 
 # library(readr) #readr is part of tidyverse, no need to call it again
 fluxes <- read_csv("fluxes_bio102.csv") %>% #give an obvious name to your objects, so that you know what it is
-  filter( #you can directly filter the data here
+  filter ( #you can directly filter the data here
     r.squared >= 0.7 #removing r.square belov 0.7, because data is not good enough for analyses
     & ID != 11 #removed because we suspect someone breath in the chamber
     & p.value <= 0.05 #we filter the p.value at that step. The non significant data should not be removed during the cleaning (my mistake, sorry)
@@ -28,7 +35,7 @@ ggplot(fluxes, aes(x= Site, y = flux)) +
   geom_jitter(shape = 15, colour = "blue") +
   scale_x_discrete() + xlab("Site") + ylab("Flux (mmol/m2/h") + 
   facet_wrap(~Type) + # makes wraps per type
-  theme_classic() # detail: figure is not super pretty, maybe something can be done here
+  theme_minimal() # detail: figure is not super pretty, maybe something can be done here
 
 
 #anova on the different type of flux
@@ -40,8 +47,6 @@ anova.fluxes <- fluxes %>%
       map(data, ~summary(aov(flux ~ Site,.)))
     )
 anova.fluxes[[3]] # will print the results of the anova
-
-
 
 
 
